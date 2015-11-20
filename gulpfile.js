20677a174f7bb610952b1ee71e9c7c8ec0cccdb9
Yoel Monzon <yoelfme@hotmail.com>
2015-11-20 15:32:12 -0600

    [add] task for compile css, and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect'),
-    history = require('connect-history-api-fallback');
+    history = require('connect-history-api-fallback'),
+    stylus = require('gulp-stylus'),
+    nib = require('nib');
 
 var historyApiFallback = history({});
 
@@ -15,3 +17,25 @@ gulp.task('server', function () {
     }
   });
 });
+
+// Compile Stylus files to CSS files and reload changes
+gulp.task('css', function () {
+  gulp.src('./app/stylesheets/main.styl')
+    .pipe(stylus({ use: nib() }))
+    .pipe(gulp.dest('./app/stylesheets'))
+    .pipe(connect.reload());
+});
+
+// Reload the web browser when html files are changed
+gulp.task('html', function () {
+  gulp.src('./app/**/*.html')
+    .pipe(connect.reload());
+});
+
+// Watch changes that occur in the code y trigger the tasks
+gulp.task('watch', function () {
+  gulp.watch(['./app/**/*.html'], ['html']);
+  gulp.watch(['./app/stylesheets/**/*.styl'], ['css']);
+});
+
+gulp.task('default', ['server', 'watch']);
